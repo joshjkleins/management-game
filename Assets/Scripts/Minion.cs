ba9ff81a39aa5af2b9ec4b317a31b5f49ca925d1
unknown <joshjkleins@gmail.com>
2023-05-09 15:20:39 -0500

    'wip'

--- Assets/Scripts/Minion.cs
+++ Assets/Scripts/Minion.cs
@@ -1,23 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Minion : MonoBehaviour
 {
+    public MissionHandler missionHandler;
+    public ScreenTransitions screenTransitions;
     public enum MinionClass {Warrior, Mage, Rogue, Priest}
+    public TMP_Text titleLevel;
 
     public MinionClass minClass;
+    public int minLevel;
 
     // Start is called before the first frame update
     void Start()
     {
+        minLevel = Random.Range(1, 15);
         minClass = (MinionClass)Random.Range(0, 3);
+        titleLevel.text = $"{minClass.ToString()}: Level {minLevel.ToString()}";
+
+        // generateMissions();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void availableMissions() {
+        missionHandler.getAvailableMissions(minLevel);
+        screenTransitions.showMissions();
+    }
+
+    void generateMissions() {
+        // generate list of available missions
+        // save data to this minion
+
+        // when button is clicked pass that data to missions object to display
     }
 
 }
